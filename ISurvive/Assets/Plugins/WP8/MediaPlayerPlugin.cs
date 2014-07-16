0e68cb6936d02f614a796ac11de9e0dfed359c4a
sukma arbianto <sukmasuk2006@gmail.com>
2014-07-16 13:14:41 +0700

    First Blood
    yeahh

---
+++ ISurvive/Assets/Plugins/WP8/MediaPlayerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using PluginUnityWP;
+
+public class MediaPlayerPlugin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+}
